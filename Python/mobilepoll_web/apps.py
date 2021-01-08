333fdfe755fb5dd97d2c561bcb4a697d0afa6ad8
Anselmo Berriel de Lira <anselmo.lira1@gmail.com>
2021-01-08 13:18:11 -0300

    Removendo o que tinha antes em Python. Vai entrar agora o código em Clean Architecture

--- Python/mobilepoll_web/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class MobilepollWebConfig(AppConfig):
-    name = 'mobilepoll_web'
--- Python/mobilepoll_web/models/interviewer_client_mail.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-from django.db import models
-from django.utils import timezone
-import uuid
